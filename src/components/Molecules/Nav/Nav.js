0bf962afdc57df607bb2c4dac424fef1b7985334
juangidoni <juan.gidoni@gmail.com>
2021-09-27 20:09:01 -0300

    #12: Removed context and created last pages.

--- src/components/Molecules/Nav/Nav.js
+++ src/components/Molecules/Nav/Nav.js
@@ -3,9 +3,9 @@ import NavList from '../../Atoms/NavList';
 const Nav = () => {
  return (
   <ul className="navbar-nav ml-auto">
-   <NavList text="Age" to="/age" />
-   <NavList text="Weight" to="/weight" />
-   <NavList text="Height" to="/height" />
+   <NavList text="Age" to="/filter/age/a" />
+   <NavList text="Weight" to="/filter/weight/a" />
+   <NavList text="Height" to="/filter/height/a" />
   </ul>
  );
 };
