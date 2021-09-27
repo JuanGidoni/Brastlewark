c10f3bbfe7a1cc1d295127fea170d819870f830a
juangidoni <juan.gidoni@gmail.com>
2021-09-27 18:38:03 -0300

    #10: Added Data Context and functionalities

--- src/components/App.js
+++ src/components/App.js
@@ -7,30 +7,36 @@ import { Footer } from "./Molecules";
 import { Navbar } from './Organisms';
 import { Layout } from "./Templates";
 import { Main } from "./Pages";
+import { DataProvider } from "./Contexts/Data";
 
 const App = () => {
   return (
     <Router>
-      <Layout className="container-fluid p-0 m-0">
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route path="/age" exact>
-            </Route>
-            <Route path="/weight" exact>
-            </Route>
-            <Route path="/height" exact>
-            </Route>
-            <Route path="/" exact>
-              <Main />
-            </Route>
-            <Route path="*">
-              <>Not found</>
-            </Route>
-          </Switch>
-          <Footer className="w-100 h-auto text-center py-1 text-primary border-top border-primary mt-1">@copyright - 2021</Footer>
-        </div>
-      </Layout>
+      <DataProvider>
+        <Layout className="container-fluid p-0 m-0">
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route path="/age" exact>
+              </Route>
+              <Route path="/weight" exact>
+              </Route>
+              <Route path="/height" exact>
+              </Route>
+              <Route path="/item/:id" exact>
+                <>Found</>
+              </Route>
+              <Route path="/" exact>
+                <Main />
+              </Route>
+              <Route path="*">
+                <>Not found</>
+              </Route>
+            </Switch>
+            <Footer className="w-100 h-auto text-center py-1 text-primary border-top border-primary mt-1">@copyright - 2021</Footer>
+          </div>
+        </Layout>
+      </DataProvider>
     </Router>
   );
 }
--- src/components/Atoms/Spinner/Spinner.js
+++ src/components/Atoms/Spinner/Spinner.js
@@ -1,7 +1,11 @@
+import './Spinner.css';
+
 const Spinner = () => {
  return (
-  <div className="spinner-border" role="status">
-   <span className="sr-only">Loading...</span>
+  <div className="spinner">
+   <div className="spinner-grow" role="status">
+    <span className="sr-only ">Loading...</span>
+   </div>
   </div>
  );
 };
